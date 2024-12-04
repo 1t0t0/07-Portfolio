2c24a3815594cdb77227a96463fe8e8da5b298d5
Toto <toto@Totos-MacBook-Pro.local>
2024-12-04 09:01:41 +0700

    contact and footer

---
+++ src/sections/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './FooterStyles.module.css'
+
+
+const Footer = () => {
+    return (
+        <section id='footer' className={styles.container}>
+            <p>&copy; 2024 Phetnikone Phandolack. <br />
+                All right reserved</p>
+        </section>
+    )
+}
+
+export default Footer
