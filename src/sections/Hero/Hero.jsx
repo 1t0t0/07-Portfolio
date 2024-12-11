18181cad90433d58bf6d6abad0198345b406ee08
Toto <toto@Totos-MacBook-Pro.local>
2024-12-11 10:09:43 +0700

    change theme

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -10,7 +10,7 @@ import igLight from '../../assets/instagramLight.png'
 import igDark from '../../assets/instagramDark.png'
 
 
-import CV from '../../assets/Resume.pdf'
+import CV from '../../assets/Resume-for-portfolio.pdf'
 import { useTheme } from '../../common/ThemeContext'
 import TypeAnimationText from '../../common/TypeAnimation'
 
--- src/sections/Project/Projects.jsx
+++ src/sections/Project/Projects.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
 import styles from './ProjectsStyles.module.css'
-import chatGPT from '../../assets/chatgpt.jpg'
+import geminiLight from '../../assets/Gemini-light.jpg'
+import geminiDark from '../../assets/Gemini-dark.jpg'
 import cryptoplace from '../../assets/CryptoPlace.webp'
 import blogapp from '../../assets/Blogapp.webp'
 import ProjectCard from '../../common/ProjectCard'
+import { useTheme } from '../../common/ThemeContext'
+
 
 
 
 const Projects = () => {
+    const { theme, toggleTheme } = useTheme()
+    const geminiIcon = theme === 'light' ? geminiDark : geminiLight
+
     return (
         <section id='projects' className={styles.container}>
             <h1 className='sectionTitle'>Project</h1>
             <div className={styles.projectsContainer}>
-                <ProjectCard src={chatGPT} link='' h3={'ChatGPT-Clone'} />
+                <ProjectCard src={geminiIcon} link='https://github.com/1t0t0/09-Gemini-Clone.git' h3={'Gemini-Clone'} />
                 <ProjectCard className={styles.projectCard} src={cryptoplace} link='https://github.com/1t0t0/08-cryptoplace.git' h3={'CryptoPlace'} />
                 <ProjectCard src={blogapp} link='https://github.com/1t0t0/03-blog-app.git' h3={'Blog-App'} />
             </div>
